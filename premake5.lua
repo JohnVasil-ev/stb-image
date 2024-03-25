4c7b765a04b9a4c144ff9cb80b04546abe433571
John Vasiliev <va3iliev.ip@gmail.com>
2024-03-25 12:33:51 +0300

    ERA only sources

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "StbImage"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "Off"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/stb_image/stb_image.h",
+        "src/stb_image.cpp"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
---
+++ src/stb_image.cpp
@@ -0,0 +1,4 @@
+#include "erapch.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
